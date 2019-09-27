7a9ba3757a6c35536c2593fb004fa9065f40ed5f
bshy <bshyman@gmail.com>
2019-09-27 00:06:46 -0500

    users controller and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/dashboard'
   resources :transactions
   resources :phones
   
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
+  root 'users#dashboard'
 end
