b5d92d3c4f55ef0d118875ea090a32ce4c2fdb7a
bshy <bshyman@gmail.com>
2019-11-08 23:02:48 -0600

    select helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def phone_models_for_select
+    {
+        'iPhone X':      :iphone_x,
+        'iPhone 8 Plus': :iphone_8_plus,
+        'iPhone XR':     :iphone_xr
+    }
+  end
+  
+  def phone_capacities_for_select
+    {
+        '16gb' =>16,
+        '64gb' =>64,
+        '128gb'=>128
+    }
+  end
+  
+  def carriers_for_select
+    {
+        Verizon: :vzw,
+        Sprint:  :sprint,
+        GSM:     :gsm,
+        ATT:     :att
+      
+    }
+  end
+  
+  def colors_for_select
+    {
+        Black:    :black,
+        'White':  :white,
+        'Silver': :silver
+    }
+  end
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -2,5 +2,10 @@ class Phone < ApplicationRecord
   has_many :phone_tranzactions
   has_many :listings
   has_and_belongs_to_many :tranzactions, through: :phone_tranzactions
+
+  def select_option
+   model.split.last.upcase + ':' + capacity + ':' + imei.to_s
+   # +color&.to_s + ':' # +' - ' + original_carrier
+  end
   
 end
