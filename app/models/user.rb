6ea4e8a1cc3db6bcd2a5da5f817e5dd0ebb28f4a
bshy <bshyman@gmail.com>
2020-02-08 17:34:30 -0600

    more user oath stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ApplicationRecord
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
-      if auth['info']
-        user.name       = auth.dig('info', 'name')
-        user.email      = auth.dig('info', 'email')
-        user.first_name = auth.dig('info', 'first_name')
-        user.last_name  = auth.dig('info', 'last_name')
-        # user.image = auth.dig('info', 'image')
-      end
+      user.name       = auth.dig('info', 'name')
+      user.email      = auth.dig('info', 'email')
+      user.first_name = auth.dig('info', 'first_name')
+      user.last_name  = auth.dig('info', 'last_name')
+      # user.image = auth.dig('info', 'image')
     end
   end
 
