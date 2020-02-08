a12b5f93981e26fd14186a0654652dbf33fec8a0
bshy <bshyman@gmail.com>
2020-02-08 16:44:42 -0600

    fix smtp config for outgoing emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
       address:              "smtp.gmail.com",
-      port:                 587,
+      port:                 465,
       domain:               'gmail.com',
       enable_starttls_auto: true,
       authentication:       "plain",
