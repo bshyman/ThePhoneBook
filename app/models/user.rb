b874948cec998465a43b58f3bb65d54804dae6ab
bshy <bshyman@gmail.com>
2020-02-08 17:25:28 -0600

    include all available fields in user create from oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
-
+  
+  validates_presence_of :email
+  
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
-
       if auth['info']
-         user.name = auth['info']['name'] || ""
+        user.name       = auth.dig('info', 'name')
+        user.email      = auth.dig('info', 'email')
+        user.first_name = auth.dig('info', 'first_name')
+        user.last_name  = auth.dig('info', 'last_name')
+        # user.image = auth.dig('info', 'image')
       end
     end
   end
